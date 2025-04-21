ecc0e89ead91c9acfb5b663233c3ac741518f689
Bugra <bugradaryal@hotmail.com>
2025-04-21 16:58:39 +0300

    .

---
+++ Utilitys/MailServices/MailSender.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MailKit.Net.Smtp;
+using MimeKit;
+
+namespace Utilitys.MailServices
+{
+    public class MailSender
+    {
+        public void Send()
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Joey Tribbiani", "joey@friends.com"));
+            message.To.Add(new MailboxAddress("Mrs. Chanandler Bong", "chandler@friends.com"));
+            message.Subject = "How you doin'?";
+
+            message.Body = new TextPart("plain")
+            {
+                Text = @"Hey Chandler,
+
+                I just wanted to let you know that Monica and I were going to go play some paintball, you in?
+
+                -- Joey"
+            };
+
+            using (var client = new SmtpClient())
+            {
+                client.Connect("smtp.friends.com", 587, false);
+
+                // Note: only needed if the SMTP server requires authentication
+                client.Authenticate("joey", "password");
+
+                client.Send(message);
+                client.Disconnect(true);
+            }
+        }
+    }
+    }
+}
